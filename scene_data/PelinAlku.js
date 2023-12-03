bc586f5c979233d4649b9766ce59b4511396f950
rikutt <115475887+rikutt@users.noreply.github.com>
2023-12-04 00:29:25 +0200

    Pelin alku eka scene nimetty uudelleen

--- scene_data/PelinAlku.js
+++ scene_data/PelinAlku.js
@@ -1,41 +1,41 @@
 const PelinAlku = {
-    PelinAlku: {
-      _comment: "",
-      type: "linear",
-      background: "KoulunPiha",
-      characters: [],
-      text_type: "narrator",
-      text: "tPelinAlku",
-      next_scene: "PelinAlkuHahmonMuokkaus"
-    },
-  
-    PelinAlkuHahmonMuokkaus: {
-      _comment: "",
-      type: "linear",
-      background: "KoulunPiha",
-      characters: [],
-      text_type: "narrator",
-      text: "tPelinAlkuHahmonMuokkaus",
-      next_scene: "PelinAlkuTutorial"
-    },
-  
-    PelinAlkuTutorial: {
-      _comment: "",
-      type: "options",
-      background: "KoulunPiha",
-      characters: [],
-      text_type: "narrator",
-      text: "tPelinAlkuTutorial",
-      player_choice: [
-        {
-          "text": "tPelinAlkuTutorialNukkumaan",
-          "next_scene": "SofillaOnTietoaAlku"
-        },
-        {
-          "text": "tPelinAlkuTutorialKasinAamut",
-          "next_scene": "SofillaOnTietoaAlku"
-        }
-      ]
-    }
+  EnsimmainenScene: {
+    _comment: "",
+    type: "linear",
+    background: "KoulunPiha",
+    characters: [],
+    text_type: "narrator",
+    text: "tPelinAlku",
+    next_scene: "PelinAlkuHahmonMuokkaus"
+  },
+
+  PelinAlkuHahmonMuokkaus: {
+    _comment: "",
+    type: "linear",
+    background: "KoulunPiha",
+    characters: [],
+    text_type: "narrator",
+    text: "tPelinAlkuHahmonMuokkaus",
+    next_scene: "PelinAlkuTutorial"
+  },
+
+  PelinAlkuTutorial: {
+    _comment: "",
+    type: "options",
+    background: "KoulunPiha",
+    characters: [],
+    text_type: "narrator",
+    text: "tPelinAlkuTutorial",
+    player_choice: [
+      {
+        "text": "tPelinAlkuTutorialNukkumaan",
+        "next_scene": "SofillaOnTietoaAlku"
+      },
+      {
+        "text": "tPelinAlkuTutorialKasinAamut",
+        "next_scene": "SofillaOnTietoaAlku"
+      }
+    ]
+  }
 }
 export default PelinAlku
