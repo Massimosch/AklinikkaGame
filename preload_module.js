d298822d776c9957eb3457967fc1dc876b1ea9c8
rikutt <115475887+rikutt@users.noreply.github.com>
2023-12-04 18:20:31 +0200

    added preload_module which should possibly work

---
+++ preload_module.js
@@ -0,0 +1,45 @@
+/*
+  Module handles preloading images
+*/
+document.addEventListener('DOMContentLoaded', function () {
+  const firstScenes = [
+    './images/backgrounds/KoulunPiha.png',
+    './images/backgrounds/Roskikset.png',
+    './images/characters/HahmoSofi.png',
+    './images/characters/HahmoSofiHuolestunut.png',
+    './images/characters/HahmoSofiVakava.png',
+    './images/characters/HahmoSofiVihainen.png',
+    './images/characters/HahmoSofiYlpea.png',
+    './images/characters/HahmoKouluterkka.png',
+    // checkataan mitkä on tärkeimmät kuvat alkuun
+  ];
+
+  const restOfTheImages = [
+    './images/backgrounds/Automaatti.png',
+    './images/backgrounds/Energiajuoma.png',
+    './images/backgrounds/EnergiaJuomaPinkki.png',
+    './images/backgrounds/JuomaAutomaattiPinkki.png',
+    './images/backgrounds/Kannabis.png',
+    // lisätään joskus loput
+  ];
+
+  let loadedImages = 0;
+  let imageObject = [];
+
+  const loadImages = (images) => {
+    images.forEach((image) => {
+      const img = new Image();
+      img.src = image;
+      img.onload = () => {
+        loadedImages++;
+        if (loadedImages === images.length) {
+          loadedImages = 0; // reset the counter
+          loadImages(restOfTheImages); // load the next batch of images
+        }
+      };
+      imageObject.push(img);
+    });
+  };
+
+  loadImages(firstScenes); // start by loading the first batch of images
+});
