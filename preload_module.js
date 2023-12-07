b2bd49f3dd17c5ffd17d0d31eb1160a1b515b592
rikutt <115475887+rikutt@users.noreply.github.com>
2023-12-07 03:22:29 +0200

    preload module, needs github pages testing

--- preload_module.js
+++ preload_module.js
@@ -2,44 +2,45 @@
   Module handles preloading images
 */
 document.addEventListener('DOMContentLoaded', function () {
-  const firstScenes = [
+  const firstScenePreload = [
     './images/backgrounds/KoulunPiha.png',
-    './images/backgrounds/Roskikset.png',
     './images/characters/HahmoSofi.png',
     './images/characters/HahmoSofiHuolestunut.png',
-    './images/characters/HahmoSofiVakava.png',
     './images/characters/HahmoSofiVihainen.png',
-    './images/characters/HahmoSofiYlpea.png',
     './images/characters/HahmoKouluterkka.png',
     // checkataan mitkä on tärkeimmät kuvat alkuun
   ];
 
   const restOfTheImages = [
-    './images/backgrounds/Automaatti.png',
     './images/backgrounds/Energiajuoma.png',
     './images/backgrounds/EnergiaJuomaPinkki.png',
-    './images/backgrounds/JuomaAutomaattiPinkki.png',
-    './images/backgrounds/Kannabis.png',
+    './images/backgrounds/KannabisLiila.png',
+    './images/backgrounds/Kaytava.png',
+    './images/backgrounds/JuomaAutomaatti.png',
+    './images/backgrounds/LuokanOvi.png',
+    './images/backgrounds/Luokkahuone.png',
+    './images/backgrounds/MironKoti.png',
+    './images/backgrounds/Nuuska.png',
+    './images/backgrounds/OmaHuone.png',
+    './images/backgrounds/Pillerit.png',
+    './images/backgrounds/PuhelinKirjoitaViesti.png',
+    './images/backgrounds/PuhelinMuskelit.png',
+    './images/backgrounds/PuhelinSnappi.png',
+    './images/backgrounds/PuhelinViestit.png',
+    './images/backgrounds/Puisto.png',
+    './images/backgrounds/Risteys.png',
+    './images/backgrounds/RoopenKoti.png',
+    './images/backgrounds/Roskikset.png',
+    './images/backgrounds/Steroidit.png',
+    './images/backgrounds/Vanhemmat.png',
     // lisätään joskus loput
   ];
+  preloadImages(restOfTheImages);
 
-  let loadedImages = 0;
-  let imageObject = [];
-
-  const loadImages = (images) => {
+  function preloadImages(images) {
     images.forEach((image) => {
       const img = new Image();
       img.src = image;
-      img.onload = () => {
-        loadedImages++;
-        if (loadedImages === images.length) {
-          loadedImages = 0; // reset the counter
-          loadImages(restOfTheImages); // load the next batch of images
-        }
-      };
-      imageObject.push(img);
     });
-  };
-
-  //loadImages(firstScenes); // start by loading the first batch of images
+  }
 });
